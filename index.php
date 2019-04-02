605268cf0d501a05ac8b7c91907e8bb49706bb66
donatasju <donatas.jurkusl@gmail.com>
2019-04-02 12:14:41 +0300

    jo jo

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php print 'veikia' ?>
+<?php 
+
+class Girl {
+    
+    public function beSmart() {
+        return 'Im Smart';
+    }
+    
+    public function beBeatiful() {
+        return 'Im Beautiful';
+    }
+}
+
+class Girlfriend extends Girl {
+    public function pistiProta() {
+        return 'Ka ten palaikinai fb?';
+    }
+}
+
+class Wife extends Girlfriend {
+    public function pistiProta() {
+        return 'Ne dabar. As pavargus';
+    }
+}
