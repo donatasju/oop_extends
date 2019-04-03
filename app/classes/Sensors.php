2447fc40cafcc9cabd8ec9216d5cba834ee3ab17
donatasju <donatas.jurkusl@gmail.com>
2019-04-03 11:49:10 +0300

    created Sensors.php class

--- app/classes/Sensors.php
+++ app/classes/Sensors.php
@@ -1,19 +1,21 @@
 <?php
+
 namespace App;
 
 class Sensors {
+
     private $data;
-    
-    public function add ($id, App\Abstracts\Sensor $sensor) {
+
+    public function add($id, App\Abstracts\Sensor $sensor) {
         $this->data[$id] = $sensor;
     }
-    
+
     public function getReading($id) {
         return $this->data[$id];
     }
-    
+
     public function getReadings() {
         return $this->data;
     }
-}
 
+}
