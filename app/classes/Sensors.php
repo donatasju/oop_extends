5ada0119464b1799cdae1578702e9769353e93f5
donatasju <donatas.jurkusl@gmail.com>
2019-04-03 12:55:39 +0300

    created SensorsFartTemp.php class

--- app/classes/Sensors.php
+++ app/classes/Sensors.php
@@ -5,17 +5,27 @@ namespace App;
 class Sensors {
 
     private $data;
+    /** @var \App\Abstracts\Sensor[] */
+    private $sensors;
+    
+    public function __construct() {
+        $this->sensors = [];
+    }
 
     public function add($id, App\Abstracts\Sensor $sensor) {
-        $this->data[$id] = $sensor;
+        $this->sensors[$id] = $sensor;
     }
 
     public function getReading($id) {
-        return $this->data[$id];
+        return $this->sensors[$id]->read();
     }
 
     public function getReadings() {
-        return $this->data;
+        foreach ($this->data as $id => $sensor) {
+            $data[$id] = $sensor->read();
+        }
+        
+        return $data;
     }
 
 }
