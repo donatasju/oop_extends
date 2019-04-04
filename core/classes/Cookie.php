83364d525c91ada0ea1bfe33a639c9481f75bc58
donatasju <donatas.jurkusl@gmail.com>
2019-04-04 11:48:53 +0300

    cookies php created

--- core/classes/Cookie.php
+++ core/classes/Cookie.php
@@ -2,4 +2,26 @@
 
 namespace Core;
 
+class Cookie extends \Core\Abstracs\Cookie {
 
+    public function __construct(string $name) {
+        
+    }
+
+    public function delete(): void {
+        
+    }
+
+    public function exists(): bool {
+        
+    }
+
+    public function read(): array {
+        
+    }
+
+    public function save($data, $expires_in = 3600): void {
+        
+    }
+
+}
