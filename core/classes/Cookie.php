af0949d5fec0840a7f40e58d9f006188030d98ba
donatasju <donatas.jurkusl@gmail.com>
2019-04-04 13:12:05 +0300

    cookie2

--- core/classes/Cookie.php
+++ core/classes/Cookie.php
@@ -13,11 +13,20 @@ class Cookie extends \Core\Abstracs\Cookie {
     }
 
     public function exists(): bool {
-        
+        return isset($_COOKIE[$this->name]);
     }
 
     public function read(): array {
-        
+        if ($this->exists()) {
+            $decode = json_decode($_COOKIE[$this->name]);
+
+            if ($decode) {
+                return $decode;
+            }
+            trigger_error('Nepavyko decodint', E_USER_WARNING);
+        }
+
+        return [];
     }
 
     public function save($data, $expires_in = 3600): void {
